0805e52367ec208dafc1718c2189ad02fff243dc
trembleyluke <42677977+trembleyluke@users.noreply.github.com>
2018-09-12 09:10:48 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("l(a\n \n \nle\naf\nfa\n \n \nll\n \n \ns)\none\nl\n \n \niness");
+	System.out.println("l(a\n \nle\naf\nfa\n \nll\n \ns)\none\nl\n \niness");
     }
 }
